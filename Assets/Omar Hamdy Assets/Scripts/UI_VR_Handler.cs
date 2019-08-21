216e271b0abcb568e85226741905a9bc2780bc6e
Omar Hamdy <omar.h.shafeek@gmail.com>
2019-08-21 10:31:14 +0200

    Change the weapon inside the scene

---
+++ Assets/Omar Hamdy Assets/Scripts/UI_VR_Handler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class UI_VR_Handler : MonoBehaviour
+{
+    private VRTK_InteractableObject Vrscript;
+    public GameObject weaponObject;
+
+    UI_VR_Handler[] SwitchWeaponBtnsHandlers;
+    public int highlightTimeThreshold = 3;
+    float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vrscript = GetComponent<VRTK_InteractableObject>();
+        SwitchWeaponBtnsHandlers = transform.parent.GetComponentsInChildren<UI_VR_Handler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countSeconds();
+        CheckForSelectionThreshold();
+    }
+
+    public void countSeconds() {
+        if (Vrscript.enabled)
+        {
+            seconds += Time.deltaTime;
+        }
+    }
+    private void CheckForSelectionThreshold()
+    {
+        if ((Vrscript.enabled && (seconds >= highlightTimeThreshold)))
+        {
+            seconds = 0;
+            weaponObject.SetActive(true);
+            turnOffOtherWeapons();
+        }
+    }
+
+    private void turnOffOtherWeapons(){
+
+        if (SwitchWeaponBtnsHandlers != null) 
+        {
+            foreach (var script in SwitchWeaponBtnsHandlers)
+            {
+                if (script != this)
+                {
+                    script.weaponObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
