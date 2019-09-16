b9b0cc6c22b1cce1d1ec93e6389c2d3885b25593
Marwan Osama EL-Safy <MarwanOsama1997@gmail.com>
2019-09-16 13:03:14 +0200

    After Importing Assembly Scene

---
+++ Assets/Omar Hamdy Assets/SceneMapping.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// Just an enum that holds the exact name of the scene.
+/// </summary>
+public enum ScenesNames {
+    Intro,
+    Home,
+    Test,
+    Shooter,
+    Tutorial,
+    Assembly
+}
+
+
